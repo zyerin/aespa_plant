c129693ec4aaa6290f9018d70a2c144fb6a45457
yubin012 <pyb2675@naver.com>
2023-04-26 16:36:35 +0900

    Initial commit

---
+++ app/src/main/java/com/example/aespa_plant/activity_login.java
@@ -0,0 +1,25 @@
+package com.example.aespa_plant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class activity_login extends AppCompatActivity {
+    TextView sign;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        sign = findViewById(R.id.signin);
+
+        sign.setOnClickListener(v->{
+            //signup 액티비티 ( 회원가입 페이지로 이동)
+            Intent intent = new Intent(this, Signup.class);
+            startActivity(intent);
+        });
+
+    }
+}
